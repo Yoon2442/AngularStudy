85ac7ca2aa34b30a1ea8efce97f406a4dc2e99e8
Sam Yoon(윤희웅) <huyoon@bistel.ai>
2022-01-25 12:30:10 +0900

    v.2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Hulk'},
+    { id: 12, name: 'BlackWidow'},
+    { id: 13, name: 'HawkEye'},
+    { id: 14, name: 'Captain America'},
+    { id: 15, name: 'IronMan'},
+    { id: 16, name: 'Thor'},
+];
